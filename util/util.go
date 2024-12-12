39492fc1413bc4967e6ddd6877129652e4b141bc
jsh2 <sh.jung2@okestro.com>
2024-12-12 12:57:19 +0900

    Add 파일 롤링을 위한 파일 이름 생성

--- util/util.go
+++ util/util.go
@@ -1,6 +1,7 @@
 package util
 
 import (
+	"fmt"
 	"time"
 
 	"go.uber.org/zap/zapcore"
@@ -11,4 +12,10 @@ func KstTimeEncoder(t time.Time, enc zapcore.PrimitiveArrayEncoder) {
 	kst, _ := time.LoadLocation("Asia/Seoul")
 	kstTime := t.In(kst)
 	enc.AppendString(kstTime.Format("2006-01-02 15:04:05"))
+}
+
+// 파일 롤링을 위한 파일 이름 생성
+func GetLogFileName() string {
+	today := time.Now().Format("2006-01-02")
+	return fmt.Sprintf("logs/%s-1.log", today)
 }
